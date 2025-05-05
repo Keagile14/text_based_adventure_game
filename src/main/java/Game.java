4d190b6f51625beee0722f234a8573459c065ef3
Keagile14 <kendulijhb024@student.wethinkcode.co.za>
2025-05-05 14:21:24 +0200

    updated code

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,6 +5,7 @@ public class Game {
 
     JFrame window;
     Container con;
+    JPanel titleNamePanel;
     public static void main(String[] args){
 
         new Game();
@@ -13,10 +14,15 @@ public class Game {
         window = new JFrame();
         window.setSize(800,600); //decides how large window is
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //It adds a function to the window
-        window.getContentPane().setBackground(Color.PINK); //we just set the colour
+        window.getContentPane().setBackground(Color.BLACK); //we just set the colour
         window.setLayout(null);
         window.setVisible(true); //even though window was created in this program, you cannot see. We need to make it appear on the screen
         con = window.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100,600,150);
+        titleNamePanel.setBackground(Color.cyan);
+        con.add(titleNamePanel);
     }
 }
 
